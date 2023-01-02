8b3eb778728bda05fc78d25652a704e1d09b57b0
KantaVD <Kantavd@gmail.com>
2023-01-02 23:19:31 +0100

    met aparte store

--- src/actions/actions.js
+++ src/actions/actions.js
@@ -1,4 +1,3 @@
-import { act } from "react-dom/test-utils"
 import * as actions from "./actiontypes"
 
 export const addDentist = (dentist) => {
--- src/index.js
+++ src/index.js
@@ -3,13 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 import { Provider } from "react-redux";
-import { createStore } from "redux"
-import allReducers from "./reducers/allReducers"
-
-const store = createStore(
-    allReducers,
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-        )
+import store from "./store";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
@@ -18,5 +12,3 @@ root.render(
     <App />
   </Provider>
 );
-
-export default store
